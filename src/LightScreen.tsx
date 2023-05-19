bf2c2fc1b7da0e2df69d0643ea2ce62e89c8e1f3
JohnDoubleUB <JohnDoubleUB@gmail.com>
2023-05-19 08:28:31 +0100

    Added in switch to new page

--- src/LightScreen.tsx
+++ src/LightScreen.tsx
@@ -1,16 +1,31 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../App";
-import { Text, View } from "react-native";
+import { Text, View, Switch } from "react-native";
+import { useState } from "react";
 
 
 type Props = NativeStackScreenProps<RootStackParamList, 'LightScreen'>;
 
 export default function LightScreen({ route, navigation }: Props): JSX.Element
 {
+    const [isEnabled, setIsEnabled] = useState(false);
 
+    function toggleSwitchHandler()
+    {
+        setIsEnabled(previousState => !previousState);
+    }
+
+    const color: string = isEnabled ? "white" : "black";
     return (
-        <View style={{ flex: 1, width: "100%", height: "100%", position: "absolute", bottom: 0 }}>
+        <View style={{ flex: 1, width: "100%", height: "100%", position: "absolute", bottom: 0, backgroundColor: color }}>
             <Text>Light Screen!</Text>
+            <Switch
+                trackColor={{ false: '#767577', true: '#81b0ff' }}
+                thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+                ios_backgroundColor="#3e3e3e"
+                onValueChange={toggleSwitchHandler}
+                value={isEnabled}
+            />
         </View>
     );
 }
